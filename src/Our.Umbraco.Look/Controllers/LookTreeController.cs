4161e5dd4dd4cb18c0f55d0476c4b53079e75cb2
Hendy Racher <git@hendyracher.co.uk>
2018-12-09 00:42:55 +0000

    added tree controller

---
+++ src/Our.Umbraco.Look/Controllers/LookTreeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Http.Formatting;
+using Umbraco.Core;
+using Umbraco.Web.Models.Trees;
+using Umbraco.Web.Trees;
+
+namespace Our.Umbraco.Look.Controllers
+{
+    [Tree("developer", "lookTree", "Look")]
+    public class LookTreeController : TreeController
+    {
+
+        protected override TreeNodeCollection GetTreeNodes(string id, FormDataCollection queryStrings)
+        {
+            var treeNodeCollection = new TreeNodeCollection();
+            
+            if (id == Constants.System.Root.ToInvariantString())
+            {
+
+                treeNodeCollection.Add(CreateTreeNode("1", "-1", queryStrings, "Look Tree Node"));
+            }
+
+            return treeNodeCollection;
+        }
+
+        protected override MenuItemCollection GetMenuForNode(string id, FormDataCollection queryStrings)
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+}
